a1aa6feb5806c580fda1348537dccbf618628272
Deepak <hdeepak5902@gmail.com>
2024-09-12 23:26:18 +0530

    sliding done

--- src/components/HomePageTitle.jsx
+++ src/components/HomePageTitle.jsx
@@ -1,62 +1,72 @@
-import Lottie from 'lottie-react';
+import Lottie from "lottie-react";
 import React, { useEffect, useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
 
 const HomePageTitle = () => {
     const [animationData, setAnimationData] = useState(null);
-  
-   
-    useEffect(()=>{
+
+    useEffect(() => {
         const fetchAnimationData = async () => {
-            const response = await fetch('https://lottie.host/c9df1460-034e-4d8e-9d1e-3414833ee605/dox91JnU7v.json');
+            const response = await fetch("https://lottie.host/c9df1460-034e-4d8e-9d1e-3414833ee605/dox91JnU7v.json");
             const data = await response.json();
             setAnimationData(data);
         };
         fetchAnimationData();
-    },[])
+    }, []);
     if (!animationData) {
         return <div>Loading...</div>;
     }
 
     return (
-        <section className='flex flex-row items-start '>
-            <section className="flex flex-col items-start gap-8">
-                <section className="flex flex-col w-[60%] gap-8">
-                <div className="flex flex-col items-start text-[4rem] font-bold -tracking-tight">
-                    <p>Streamline</p>
-                    <p>your Schedule</p>
-                </div>
-                <div>
-                    <Lottie animationData={animationData} loop={true} autoplay={true}style={{ width: '50%', height: '50%' }} />
-                </div>
-                <p className="max-w-[90%] text-gray-700 text-lg font-medium">
-                    Effortlessly manage and book staff appointments with just a few taps. Wheather you are booking a meeting, a consulation or any other type of appointment, our intuitive platform
-                    makes it quick and simple. Say goodbye to scheduling conflicts and hello to seamless organization.
-                </p>
+        <section className="flex flex-row items-start justify-between overflow-hidden">
+            <section className="flex flex-col w-[60%]">
+                <section className="flex flex-col items-start gap-8">
+                    <div className="flex flex-col items-start text-[4rem] font-bold -tracking-tight">
+                        <p>Streamline</p>
+                        <p>your Schedule</p>
+                    </div>
+                    <div>
+                        <Lottie animationData={animationData} loop={true} autoplay={true} style={{ width: "40%", height: "40%" }} />
+                    </div>
+                    <p className="max-w-[90%] text-gray-700 text-lg font-medium">
+                        Effortlessly manage and book staff appointments with just a few taps. Whether you are booking a meeting, a consultation, or any other type of appointment, our intuitive
+                        platform makes it quick and simple. Say goodbye to scheduling conflicts and hello to seamless organization.
+                    </p>
+                    <button className="flex items-center bg-dark rounded-full flex-row text-light px-6 gap-2 text-center text-lg py-3">
+                        <FaArrowRight /> Get Started
+                    </button>
                 </section>
-                <button className="flex items-center bg-dark rounded-full flex-row text-light px-6 gap-2 text-center  text-lg py-3">
-                    <FaArrowRight /> Get Started
-                </button>
             </section>
-
-            {/* code for slide  */}
-            <section  className=' flex flex-row gap-4 overflow-hidden'> 
-            <div className='slide1 flex flex-col gap-2 overflow-hidden'>
-                <div className='pfpslide flex flex-col items-end gap-2 mb-0 '>
-                    <LazyLoadImage src='/pfp.webp' className='  h-[30%] w-[60%] rounded-md '/>
-                    <LazyLoadImage src='/pfp.webp' className=' h-[30%] w-[60%] rounded-md '/>
-                    <LazyLoadImage src='/pfp.webp' className=' h-[30%] w-[60%] rounded-md '/>
-                </div>
-                <div className='pfpslide flex flex-col items-end gap-2 mt-0  '>
-                    <LazyLoadImage src='/pfp.webp' className='h-[30%] w-[60%] rounded-md '/>
-                    <LazyLoadImage src='/pfp.webp' className=' h-[30%] w-[60%] rounded-md '/>
-                    <LazyLoadImage src='/pfp.webp' className=' h-[30%] w-[60%] rounded-md '/>
-                </div>
-                </div>
+            <section className="w-[40%] h-[80vh] flex gap-4 ">
+                <section className="flex flex-col gap-12  overflow-hidden">
+                    <div className="animate-loop-scroll-down gap-12 flex flex-col">
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                    </div>
+                    <div className="animate-loop-scroll-down gap-12 flex flex-col" aria-hidden="true">
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                    </div>
+                </section>
+                <section className="flex flex-col gap-12 overflow-hidden">
+                    <div className="animate-loop-scroll-up gap-12 flex flex-col">
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                    </div>
+                    <div className="animate-loop-scroll-up gap-12 flex flex-col" aria-hidden="true">
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                        <img src="/lake.jpg" alt="" className="max-w-none" />
+                    </div>
+                </section>
             </section>
-            
         </section>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    screens:{
-        "sm" : "425px",
-        "md" : "768px",
-        "lg" : "1024px",
-        "xl" : "1280px",
-        "2xl" : "1440px",
-      },
-    extend: {
-      screens:{
-        "ssm" : "320px",
-      },
-      colors:{
-        light: "white",
-        dark : "#1b1b1b",
-        lightgrey: "#e6e4e4",
-        lightblack: "#57636c"
-      },
+    darkMode: "class",
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        screens: {
+            sm: "425px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1440px",
+        },
+        extend: {
+            screens: {
+                ssm: "320px",
+            },
+            animation: {
+                "loop-scroll-down": "loop-scroll-down 10s linear infinite",
+                "loop-scroll-up": "loop-scroll-up 10s linear infinite",
+            },
+            keyframes: {
+                "loop-scroll-down": {
+                    from: { transform: "translateY(0)" },
+                    to: { transform: "translateY(-50%)" },
+                },
+                "loop-scroll-up": {
+                    from: { transform: "translateY(-50%)" },
+                    to: { transform: "translateY(0)" },
+                },
+            },
+            colors: {
+                light: "white",
+                dark: "#1b1b1b",
+                lightgrey: "#e6e4e4",
+                lightblack: "#57636c",
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
