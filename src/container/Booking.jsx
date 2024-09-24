f50fa16f5fc676269369cf86fc0c2dc01c816ece
Deepak <hdeepak5902@gmail.com>
2024-09-24 23:19:21 +0530

    need to make responsive

--- src/container/Booking.jsx
+++ src/container/Booking.jsx
@@ -1,44 +1,52 @@
 import React from "react";
+import { lazy } from "react";
+
+const Navbar = lazy(() => import("../components/Navbar"));
+const Footer = lazy(() => import("../components/Footer"));
 
 const Booking = () => {
     return (
         <section className="flex flex-col  min-h-screen w-full">
-            <h1>Book Your Appointment</h1>
-            <section>
-                <div>
-                    <div>
-                        <p>Select Department</p>
-                        <select name="" id="">
-                            <option value="">MCA</option>
-                        </select>
+            <Navbar />
+            <section className="xl:px-12 lg:px-8 sm:px-4 ssm:px-2 flex flex-col gap-12 py-8">
+                <h1 className="md:text-[4rem] sm:text-[3rem] ssm:text-[2.5rem] font-bold -tracking-tight">Book Your Appointment</h1>
+                <section className="flex items-center justify-between w-full p-4">
+                    <div className="flex flex-col gap-12 w-[50%]">
+                        <div className="flex flex-col items-start gap-2 w-full">
+                            <p className="text-2xl font-medium">Select Department</p>
+                            <select name="" id="" className="border-dark border-2 p-4 w-[70%] rounded-lg text-lg font-medium">
+                                <option value="">MCA</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col items-start gap-2 w-full">
+                            <p className="text-2xl font-medium">Select Staff</p>
+                            <select name="" id="" className="border-dark border-2 p-4 w-[70%] rounded-lg text-lg font-medium">
+                                <option value="">Abdur Raheem</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col items-start gap-2 w-full">
+                            <p className="text-2xl font-medium">Meeting Date</p>
+                            <input type="date" className="border-dark border-2 p-4 w-[70%] rounded-lg text-lg font-medium" />
+                        </div>
+                        <div>
+                            <p className="text-2xl font-medium">Schedule Time</p>
+                            <input type="time" className="border-dark border-2 p-4 w-[70%] rounded-lg text-lg font-medium" />
+                        </div>
                     </div>
-                    <div>
-                        <p>Select Staff</p>
-                        <select name="" id="">
-                            <option value="">Abdur Raheem</option>
-                        </select>
+                    <div className="w-[50%] flex items-center justify-center">
+                        <img src="/img13.jpg" alt="" className="w-[30rem]" />
                     </div>
-                    <div>
-                        <p>Meeting Date</p>
-                        <input type="date" />
-                    </div>
-                    <div>
-                        <p>Schedule Time</p>
-                        <input type="time" />
-                    </div>
+                </section>
+                <div className="flex flex-col px-4 gap-2 items-start w-full">
+                    <p className="text-2xl font-medium">Objective of the meet</p>
+                    <textarea name="purpose" id="" cols="30" rows="5" placeholder="Objective for your meet" className="border-dark border-2 p-4 w-full rounded-lg text-lg font-medium" />
                 </div>
-                <div>
-                    <img src="/img13.jpg" alt="" />
+                <div className="flex px-4 justify-between mb-[5rem]">
+                    <button className="bg-light outline outline-dark text-2xl px-12 py-2 rounded-full">Cancel</button>
+                    <button className="bg-dark text-light dark:text-dark dark:bg-light text-2xl px-12 py-2 rounded-full">Schedule Now</button>
                 </div>
             </section>
-            <div>
-                <p>Objective of the meet</p>
-                <textarea name="purpose" id="" cols="30" rows="10" placeholder="Objective for your meet" />
-            </div>
-            <div>
-                <button>cancel</button>
-                <button>Schedule now</button>
-            </div>
+            <Footer />
         </section>
     );
 };
