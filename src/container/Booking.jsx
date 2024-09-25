6ccc39b80ed350e21a39e5134c7a87da07a9e9cd
Deepak <hdeepak5902@gmail.com>
2024-09-25 22:56:06 +0530

    Completed Booking page

--- src/container/Booking.jsx
+++ src/container/Booking.jsx
@@ -20,44 +20,51 @@ const Booking = () => {
     }, []);
 
     return (
-        <section className="flex flex-col  min-h-screen w-full">
+        <section className="flex flex-col  min-h-screen w-full bg-light dark:bg-dark text-dark dark:text-light">
             <Navbar />
             <section className="xl:px-12 lg:px-8 sm:px-4 ssm:px-2 flex flex-col gap-12 py-8">
-                <h1 className="md:text-[4rem] sm:text-[3rem] ssm:text-[2.5rem] font-bold -tracking-tight">Book Your Appointment</h1>
-                <section className="flex items-center justify-between w-full p-4 lg:flex-row flex-col">
-                    <div className="flex flex-col gap-12 w-[50%] lg:order-1 order-2">
-                        <div className="flex flex-col items-start gap-2 w-full">
+                <h1 className="md:text-[4rem] sm:text-[3rem] ssm:text-[2.5rem] font-bold -tracking-tight lg:text-start text-center">Book Your Appointment</h1>
+                <section className="flex items-center justify-between w-full p-4 lg:flex-row flex-col gap-12">
+                    <div className="flex flex-col gap-12 lg:w-[50%] w-full lg:items-start lg:order-1 order-2">
+                        <div className="flex flex-col lg:items-start items-center gap-2 w-full">
                             <p className="text-2xl font-medium">Select Department</p>
-                            <select name="" id="" className="border-dark border-2 p-4 w-[70%] rounded-lg text-lg font-medium">
+                            <select name="" id="" className="border-dark border-2 p-4 lg:w-[70%] w-[90%] text-dark  rounded-lg text-lg font-medium focus:outline-none">
                                 <option value="">MCA</option>
                             </select>
                         </div>
-                        <div className="flex flex-col items-start gap-2 w-full">
+                        <div className="flex flex-col lg:items-start items-center gap-2 lg:w-full">
                             <p className="text-2xl font-medium">Select Staff</p>
-                            <select name="" id="" className="border-dark border-2 p-4 w-[70%] rounded-lg text-lg font-medium">
+                            <select name="" id="" className="border-dark border-2 p-4 lg:w-[70%] w-[90%]  rounded-lg text-lg font-medium text-dark focus:outline-none">
                                 <option value="">Abdur Raheem</option>
                             </select>
                         </div>
-                        <div className="flex flex-col items-start gap-2 w-full">
+                        <div className="flex flex-col lg:items-start items-center gap-2 lg:w-full ">
                             <p className="text-2xl font-medium">Meeting Date</p>
-                            <input type="date" className="border-dark border-2 p-4 w-[70%] rounded-lg text-lg font-medium" />
+                            <input type="date" className="border-dark border-2 p-4 lg:w-[70%] w-[90%]  rounded-lg text-lg font-medium text-dark focus:outline-none" />
                         </div>
-                        <div>
+                        <div className="flex flex-col lg:items-start items-center gap-2 lg:w-full ">
                             <p className="text-2xl font-medium">Schedule Time</p>
-                            <input type="time" className="border-dark border-2 p-4 w-[70%] rounded-lg text-lg font-medium" />
+                            <input type="time" className="border-dark border-2 p-4 lg:w-[70%] w-[90%]  rounded-lg text-lg font-medium text-dark focus:outline-none" />
                         </div>
                     </div>
-                    <div className="w-[50%] flex items-center justify-center lg:order-2 order-1">
+                    <div className="lg:w-[50%] w-full flex items-center justify-center lg:order-2 order-1">
                         <Lottie animationData={animationData} loop={true} autoplay={true} />
                     </div>
                 </section>
-                <div className="flex flex-col px-4 gap-2 items-start w-full">
+                <div className="flex flex-col px-4 gap-2 lg:items-start items-center w-full">
                     <p className="text-2xl font-medium">Objective of the meet</p>
-                    <textarea name="purpose" id="" cols="30" rows="5" placeholder="Objective for your meet" className="border-dark border-2 p-4 w-full rounded-lg text-lg font-medium" />
+                    <textarea
+                        name="purpose"
+                        id=""
+                        cols="30"
+                        rows="5"
+                        placeholder="Objective for your meet"
+                        className="border-dark border-2 p-4 w-full rounded-lg text-lg font-medium text-dark focus:outline-none"
+                    />
                 </div>
-                <div className="flex px-4 justify-between mb-[5rem]">
-                    <button className="bg-light outline outline-dark text-xl px-12 py-2 rounded-full">Cancel</button>
-                    <button className="bg-dark text-light dark:text-dark dark:bg-light text-xl px-12 py-2 rounded-full">Schedule Now</button>
+                <div className="flex lg:flex-row flex-col items-center gap-12 px-4 justify-between mb-[5rem]">
+                    <button className="bg-light text-dark font-medium outline outline-dark text-xl px-12 py-2 rounded-full">Cancel</button>
+                    <button className="bg-dark text-light dark:text-dark font-medium dark:bg-light text-xl px-12 py-2 rounded-full">Schedule</button>
                 </div>
             </section>
             <Footer />
